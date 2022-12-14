1c4f08f99ca18bc4503a183b49acd75f06f9a291
isaacsaju <isaacsaju9@gmail.com>
2022-12-14 15:14:56 -0800

    fix bug

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,13 +2,14 @@
 int main(void)
 {
 	char name[8] = "_putchar";
-       	int i=0;
-       	for(;i<8; i++)
+	int i = 0;
+
+	for (; i < 8; i++)
 	{
 		_putchar(name[i]);
 	}
 	_putchar('\n');
 
-	return(0);
+	return (0);
 }
 
