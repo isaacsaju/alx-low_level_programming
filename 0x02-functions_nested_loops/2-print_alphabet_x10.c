d87b093402a8007dc33652879c208cc0d1c16cc4
isaacsaju <isaacsaju9@gmail.com>
2022-12-14 17:25:44 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**a function that prints 10 times the alphabet,
+ * in lowercase, followed by a new line
+ **/
+void print_alphabet_x10(void)
+{
+	char latter;
+	int i;
+	for (i=0; i<=10; i++)
+	{
+
+		for (latter='a'; latter<='z'; latter++)
+		{
+			_putchar(latter);
+		}
+		
+		_putchar('\n');
+	}
+	
+}
