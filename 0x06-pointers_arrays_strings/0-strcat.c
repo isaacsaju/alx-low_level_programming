ddb652e3250832da168ba11e74e2b2385ba15c59
isaacsaju <isaacsaju9@gmail.com>
2022-12-22 16:15:22 -0800

    src cat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcat - a function that concatenate two strings
+ * and the adds a terminating null byte.
+ *
+ * @dest: An input string
+ *
+ * @src: An input string
+ *
+ * Return: the resulting dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *temp = dest;
+
+	while (*dest)
+	{
+		dest++;
+	}
+
+	while (*src)
+	{
+		*dest++ = *src++;
+	}
+	
+	*dest = '\0';
+
+	return (temp);
+}
