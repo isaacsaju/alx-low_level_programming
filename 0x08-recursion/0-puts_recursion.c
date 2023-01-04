7a82c15c1324374efd8626601c30cf20186b2b78
isaacsaju <117784436+isaacsaju@users.noreply.github.com>
2023-01-05 00:57:04 +0100

    Add files via upload

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function that prints a string follow
+ * by a new line.
+ * @s: input string
+ * Return: Nothing
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
